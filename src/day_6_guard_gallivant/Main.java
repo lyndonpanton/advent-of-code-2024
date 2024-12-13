ace547630e9d2981cb595d1d744c1c49cfa70698
Lyndon Panton <LyndonPanton@gmail.com>
2024-12-13 17:11:36 +0000

    Created a class for managing the guard's state

--- src/day_6_guard_gallivant/Main.java
+++ src/day_6_guard_gallivant/Main.java
@@ -14,6 +14,66 @@ public class Main {
         int distinctLocations = getDistinctLocations(map);
     }
 
+    public static int getDistinctLocations(List<String> map) {
+        int locations = 0;
+        char newLocationIcon = '.';
+        char oldLocationIcon = 'X';
+        char obstacleIcon = '#';
+        List<Character> guardIcons = Arrays.asList('^', '>', 'v', '<');
+
+        Guard guard = getGuardInformation(map, guardIcons);
+
+        System.out.println("Guard location: "
+                + "("
+                + guard.getLocation()[0] + ", " + guard.getLocation()[1]
+                + ")"
+        );
+
+        System.out.println("Guard direction: " + guard.getDirection());
+
+        return locations;
+    }
+
+    public static Guard getGuardInformation(List<String> map, List<Character> icons) {
+        Guard guard = new Guard();
+
+//        int[] currentGuardLocation = new int[2];
+        int currentGuardIcon = 0;
+
+        for (int i = 0; i < map.size(); i++) {
+            boolean guardFound = false;
+
+            for (Character c: icons) {
+                if (map.get(i).contains(c.toString())) {
+                    guardFound = true;
+                    guard.setLocation(i, map.get(i).indexOf(c));
+
+                    switch (icons.indexOf(c)) {
+                        case 0:
+                            guard.setDirection(GuardDirection.Up);
+                            break;
+                        case 1:
+                            guard.setDirection(GuardDirection.Right);
+                            break;
+                        case 2:
+                            guard.setDirection(GuardDirection.Down);
+                            break;
+                        case 3:
+                            guard.setDirection(GuardDirection.Left);
+                            break;
+                    }
+                }
+
+                if (guardFound) {
+                    break;
+                }
+            }
+        }
+
+        return guard;
+    }
+
+
     public static List<String> getMapInformation(String filename) {
         BufferedReader br;
         List<String> map = new ArrayList<>();
@@ -33,41 +93,34 @@ public class Main {
 
         return map;
     }
+}
 
-    public static int getDistinctLocations(List<String> map) {
-        int locations = 0;
-        char newLocationIcon = '.';
-        char oldLocationIcon = 'X';
-        char obstacleIcon = '#';
-        List<Character> guardIcons = Arrays.asList('^', '>', 'v', '<');
+class Guard {
+    private final int[] location;
+    private GuardDirection direction;
 
-        int[] currentGuardLocation = new int[2];
-        int currentGuardIcon = 0;
-
-        for (int i = 0; i < map.size(); i++) {
-            boolean guardFound = false;
-
-            for (Character c: guardIcons) {
-                if (map.get(i).contains(c.toString())) {
-                    currentGuardLocation[0] = i;
-                    currentGuardLocation[1] = map.get(i).indexOf(c);
-
-                    currentGuardIcon = guardIcons.indexOf(c);
-                }
-            }
-        }
-
-        System.out.println(
-                "Guard location: ("
-                        + currentGuardLocation[0] + ", "
-                        + currentGuardLocation[1]
-                + ")"
-        );
-
-        System.out.println("Current guard icon: " + currentGuardIcon + " ("
-                + guardIcons.get(currentGuardIcon) + ")"
-        );
-
-        return locations;
+    public Guard() {
+        location = new int[2];
     }
+
+    public void setDirection(GuardDirection direction) {
+        this.direction = direction;
+    }
+
+    public void setLocation(int row, int column) {
+        location[0] = row;
+        location[1] = column;
+    }
+
+    public GuardDirection getDirection() {
+        return direction;
+    }
+
+    public int[] getLocation() {
+        return location;
+    }
+}
+
+enum GuardDirection {
+    Up, Right, Down, Left
 }

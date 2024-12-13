9e757c8c469baa9b4dd510bdb94854c938ff77f5
Lyndon Panton <LyndonPanton@gmail.com>
2024-12-13 16:37:43 +0000

    Extracted file data

--- src/day_6_guard_gallivant/Main.java
+++ src/day_6_guard_gallivant/Main.java
@@ -1,7 +1,35 @@
 package day_6_guard_gallivant;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Advent of Code 2024!");
+        List<String> map = getMapInformation("./src/day_6_guard_gallivant/input.txt");
+
+    }
+
+    public static List<String> getMapInformation(String filename) {
+        BufferedReader br;
+        List<String> map = new ArrayList<>();
+
+        try {
+            br = new BufferedReader(new FileReader(filename));
+            String line = br.readLine();
+
+            while (line != null) {
+                map.add(line);
+
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return map;
     }
 }

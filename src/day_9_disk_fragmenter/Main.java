3df6115336bc90bb846f92d95d59498478f40847
Lyndon Panton <LyndonPanton@gmail.com>
2024-12-16 01:28:58 +0000

    Output the individual block representation of the disk map

--- src/day_9_disk_fragmenter/Main.java
+++ src/day_9_disk_fragmenter/Main.java
@@ -11,14 +11,37 @@ public class Main {
         System.out.println("Hello Advent of Code 2024!");
         String filename = "./src/day_9_disk_fragmenter/input.txt";
         String diskMap = getDiskMap(filename);
+
+        // Change id map and free space map to a list (use the list indexes for
+        // ids)
         Map<Integer, Integer> idMap = getIDMap(diskMap);
         Map<Integer, Integer> freeSpaceMap = getFreeSpaceMap(diskMap);
 
-        for (Map.Entry<Integer, Integer> entry: freeSpaceMap.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
+        char freeSpaceCharacter = '.';
+        String individualBlockRepresentation = getIndividualBlockRepresentation(
+                idMap,
+                freeSpaceMap,
+                freeSpaceCharacter
+        );
+
+        System.out.println(individualBlockRepresentation);
 
         // Use a boolean and keep switching between values when getting output
+    }
+
+    public static String getDiskMap(String filename) {
+        BufferedReader br;
+        String map = "";
+
+        try {
+            br = new BufferedReader(new FileReader(filename));
+
+            map = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return map;
     }
 
     public static Map<Integer, Integer> getFreeSpaceMap(String diskMap) {
@@ -32,21 +55,6 @@ public class Main {
 
             j += 2;
             i++;
-        }
-
-        return map;
-    }
-
-    public static String getDiskMap(String filename) {
-        BufferedReader br;
-        String map = "";
-
-        try {
-            br = new BufferedReader(new FileReader(filename));
-
-            map = br.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
         return map;
@@ -66,5 +74,28 @@ public class Main {
         }
 
         return map;
+    }
+
+    public static String getIndividualBlockRepresentation(
+            Map<Integer, Integer> idMap,
+            Map<Integer, Integer> freeSpaceMap,
+            char freeSpaceCharacter
+    ) {
+        StringBuilder representation = new StringBuilder();
+
+        for (int i = 0; i < idMap.size(); i++) {
+            representation.append(String.valueOf(i).repeat(idMap.get(i)));
+
+            // Id map will either be the same length or 1 longer than the free
+            // space map
+            if (i < freeSpaceMap.size()) {
+                representation.append(
+                        String.valueOf(freeSpaceCharacter)
+                                .repeat(freeSpaceMap.get(i))
+                );
+            }
+        }
+
+        return representation.toString();
     }
 }

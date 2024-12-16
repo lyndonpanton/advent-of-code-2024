9ab574f9a1c149a0c00132304d3c93e097d629f4
Lyndon Panton <LyndonPanton@gmail.com>
2024-12-16 21:18:24 +0000

    Exracted file information

--- src/day_11_plutonian_pebbles/Main.java
+++ src/day_11_plutonian_pebbles/Main.java
@@ -1,7 +1,40 @@
 package day_11_plutonian_pebbles;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Advent of Code 2024!");
+        String filename = "./src/day_11_plutonian_pebbles/input.txt";
+
+        List<Long> stoneArrangement = getInitialStoneArrangement(filename);
+
+        for (Long stone: stoneArrangement) {
+            System.out.print(stone + " ");
+        }
+    }
+
+    public static List<Long> getInitialStoneArrangement(String file) {
+        List<Long> stoneArrangement = new ArrayList<>();
+
+        BufferedReader br;
+
+        try {
+            br = new BufferedReader(new FileReader(file));
+            String[] line = br.readLine().split(" ");
+
+            for (String stone: line) {
+                stoneArrangement.add(Long.parseLong(stone));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return stoneArrangement;
     }
 }

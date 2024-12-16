d3ea8acb72401ed17e9db29372e14dbbc59861e6
Lyndon Panton <LyndonPanton@gmail.com>
2024-12-16 01:09:33 +0000

    Extracted the id numbers from the disk map

--- src/day_9_disk_fragmenter/Main.java
+++ src/day_9_disk_fragmenter/Main.java
@@ -3,14 +3,21 @@ package day_9_disk_fragmenter;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Advent of Code 2024!");
         String filename = "./src/day_9_disk_fragmenter/input.txt";
         String diskMap = getDiskMap(filename);
+        Map<Integer, Integer> idMap = getIDMap(diskMap);
 
-        System.out.println(diskMap);
+        for (Map.Entry<Integer, Integer> entry: idMap.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+        // Use a boolean and keep switching between values when getting output
     }
 
     public static String getDiskMap(String filename) {
@@ -23,6 +30,21 @@ public class Main {
             map = br.readLine();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        return map;
+    }
+
+    public static Map<Integer, Integer> getIDMap(String diskMap) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int j = 0;
+        int i = 0;
+
+        while (j < diskMap.length()) {
+            map.put(i, Integer.parseInt(String.valueOf(diskMap.charAt(j))));
+            j += 2;
+            i++;
         }
 
         return map;
